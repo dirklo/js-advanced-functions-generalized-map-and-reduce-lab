29d9836dcbd5091d894a052186fbb8a1c5e47cb5
Rick <dirklo@gmail.com>
2021-03-18 14:21:08 +0100

    commit tree 0e077f7874365fc5bf44db29b760976b4007e2f4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+const map = function(array, fn) {
+    const final = [];
+    for (let item of array) {
+        final.push(fn(item))
+    };
+    return final;
+}
+
+const reduce = function(array, fn, start) {
+    let final = (!!start) ? start : array[0];
+    let i = (!!start) ? 0 : 1
+    for (; i < array.length; i++) {
+        final = fn(array[i], final)
+    };
+    return final
+}
